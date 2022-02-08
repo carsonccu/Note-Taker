d3fee41931103d613201f63c5dfe2d6b357a0a12
Carson <carsoncolgateccu@gmail.com>
2022-02-08 10:03:32 -0700

    added my post route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const fs = require ('fs');
+const fs = require('fs');
 const uniqid = require('uniqid');
 const db = require('../db/db.json');
 const { readFromFile, writeToFile, readAndAppend } = require('./helpers');
@@ -7,13 +7,26 @@ const { readFromFile, writeToFile, readAndAppend } = require('./helpers');
 
 // get req notes
 
-router.get ("/notes", (req, res) =>{
+router.get("/notes", (req, res) => {
     const dbRoute = path.join(__dirname, '../db/db.json')
     console.log(dbRoute)
     readFromFile(dbRoute)
-    .then((data) =>{
-        res.json(JSON.parse(data));
-    }).catch((err) =>{
-        console.log(err)
-    })
+        .then((data) => {
+            res.json(JSON.parse(data));
+        }).catch((err) => {
+            console.log(err)
+        })
+})
+
+// get post
+
+router.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    const newnote = {
+        title,
+        text,
+        id: uniqid(),
+    };
+    readAndAppend(newNote, './db/db.json');
+    res.json(newnote);
 })

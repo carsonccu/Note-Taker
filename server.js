1aa79c940068ff696f318e8988cde1be338f99c1
Carson <carsoncolgateccu@gmail.com>
2022-02-08 10:50:30 -0700

    having problems with server js file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const path = require('path');
+const htmlRoute = require('./route/html')
+const apiRoute = require('./route/api');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// link js and css
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(apiRoute);
+app.use(htmlRoute);
+// which port your listening on
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
